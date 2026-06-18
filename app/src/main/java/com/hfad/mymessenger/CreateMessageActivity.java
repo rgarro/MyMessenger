b66c5af9795e83098baa8c4f7f5badd0a5be5838
Rolando <rgarro@gmail.com>
2026-06-18 17:37:05 -0600

    dear old stockholm ...

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -2,7 +2,24 @@ package com.hfad.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View;
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * Hone Creek has a secret passage to Cartago
+ * The Matina mutinners got hanged
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class CreateMessageActivity extends Activity {
 
     @Override
@@ -10,4 +27,5 @@ public class CreateMessageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
     }
+
 }
