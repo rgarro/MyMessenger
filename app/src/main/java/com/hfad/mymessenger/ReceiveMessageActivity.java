add21c32476ae527539bc668f5ac19f37a10dcc3
Rolando <rgarro@gmail.com>
2026-06-18 18:11:30 -0600

    suzuki hg 425 e

---
+++ app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,29 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+public class ReceiveMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
